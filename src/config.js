016dae1fae51d6df31ba52998869fb6c4be1c67d
Taylor Burrington <taylor.burrington@gmail.com>
2019-10-31 13:54:09 -0700

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "https://s3.console.aws.amazon.com/s3/buckets/notes-app-api-prod-serverlessdeploymentbucket-6dz0xvjlt8ir/?region=us-east-2&tab=overview",
+      BUCKET: "us-east-2"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://ej3pptzdl9.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_wL1c1raxM",
+      APP_CLIENT_ID: "4l2mtchsk3p90bjk36fnfvbd8i",
+      IDENTITY_POOL_ID: "us-east-2:5e90224b-bce5-48f0-85ec-05d447968307"
+    }
+  };
